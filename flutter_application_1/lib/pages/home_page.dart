b1e9df93abd221cf092d532991244cd4b95f3dbe
Yanai99 <yanaiparan@gmail.com>
2024-04-13 20:15:40 +0300

    step 5 completed

--- flutter_application_1/lib/pages/home_page.dart
+++ flutter_application_1/lib/pages/home_page.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:flutter_application_1/constants/colors.dart';
 import 'package:flutter_application_1/constants/nav_items.dart';
 import 'package:flutter_application_1/widgets/site_logo.dart';
 
+import '../constants/size.dart';
 import '../styles/style.dart';
 import '../widgets/drawer_mobile.dart';
 import '../widgets/header_desktop.dart';
@@ -20,44 +22,50 @@ class _HomePageState extends State<HomePage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      key:scaffoldKey,
-      backgroundColor: CustomColor.scaffoldBg,
-      endDrawer: const DrawerMobile(),
-      body:ListView(
-        scrollDirection: Axis.vertical,
-        children:[
-          // Main
-          //HeaderDesktop(),
-          HeaderMobile(
-            onLogoTap: (){},
-            onMenuTap: (){
-              scaffoldKey.currentState?.openEndDrawer();
-            },
-          ),
-           // Skills
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-          ),
-           // Projects
-          Container(
-            height: 500,
-            width: double.maxFinite,
-          ),
-           // Contacts
-          Container(
-            height: 500,
-            width: double.maxFinite,
-            color: Colors.blueGrey,
-          ),
-          // Footer
-          Container(
-            height: 500,
-            width: double.maxFinite,
-          ),
-        ] ,)
+    return LayoutBuilder(
+      builder: (context,constraints) {
+        return Scaffold(
+          key:scaffoldKey,
+          backgroundColor: CustomColor.scaffoldBg,
+          endDrawer: constraints.maxWidth >= kMinDesktopWidth ? null:const DrawerMobile(),
+          body:ListView(
+            scrollDirection: Axis.vertical,
+            children:[
+              // Main
+              if(constraints.maxWidth >= kMinDesktopWidth)
+                const HeaderDesktop() 
+              else
+              HeaderMobile(
+                onLogoTap: (){},
+                onMenuTap: (){
+                  scaffoldKey.currentState?.openEndDrawer();
+                },
+              ),
+               // Skills
+              Container(
+                height: 500,
+                width: double.maxFinite,
+                color: Colors.blueGrey,
+              ),
+               // Projects
+              Container(
+                height: 500,
+                width: double.maxFinite,
+              ),
+               // Contacts
+              Container(
+                height: 500,
+                width: double.maxFinite,
+                color: Colors.blueGrey,
+              ),
+              // Footer
+              Container(
+                height: 500,
+                width: double.maxFinite,
+              ),
+            ] ,)
+        );
+      }
     );
   }
 }
