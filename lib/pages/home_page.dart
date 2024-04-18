4b768f1c8e9718d36821b500109a0e8de48e3f9c
Yanai99 <yanaiparan@gmail.com>
2024-04-18 19:01:59 +0300

    step 14 complete

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,29 +1,20 @@
-import 'dart:ffi';
-
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/painting.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_application_1/constants/colors.dart';
-import 'package:flutter_application_1/constants/nav_items.dart';
-import 'package:flutter_application_1/constants/skill_items.dart';
-import 'package:flutter_application_1/utils/project_utils.dart';
+import 'package:flutter_application_1/constants/sns_link.dart';
 import 'package:flutter_application_1/widgets/contacts_section.dart';
 import 'package:flutter_application_1/widgets/footer.dart';
 import 'package:flutter_application_1/widgets/main_mobile.dart';
-import 'package:flutter_application_1/widgets/site_logo.dart';
 import 'package:flutter_application_1/widgets/skill_mobile.dart';
-
+import 'dart:js' as js;
 import '../constants/size.dart';
-import '../styles/style.dart';
-import '../widgets/custom_text_field.dart';
 import '../widgets/drawer_mobile.dart';
 import '../widgets/header_desktop.dart';
 import '../widgets/header_mobile.dart';
 import '../widgets/main_desktop.dart';
-import '../constants/skill_items.dart';
-import '../widgets/project_card.dart';
 import '../widgets/projects_section.dart';
 import '../widgets/skill_desktop.dart';
 
@@ -37,13 +28,13 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   final scaffoldKey = GlobalKey<ScaffoldState>();
   final scrollController = ScrollController();
+
   final List<GlobalKey> navbarKeys=List.generate(4, (index) => GlobalKey());
 
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
     final screenWidth = screenSize.width;
-    final screenHeight = screenSize.height;
 
     return LayoutBuilder(builder: (context, constraints) {
       return Scaffold(
@@ -51,16 +42,21 @@ class _HomePageState extends State<HomePage> {
           backgroundColor: CustomColor.scaffoldBg,
           endDrawer: constraints.maxWidth >= kMinDesktopWidth
               ? null
-              : const DrawerMobile(),
+              : DrawerMobile(onNavItemTap: (int navIndex,){
+                  scaffoldKey.currentState?.closeEndDrawer();
+                  scrollToSection(navIndex);
+              }),
           body: SingleChildScrollView(
             controller: scrollController,
             scrollDirection: Axis.vertical,
             child: Column(
               children: [
-                SizedBox(key:navbarKeys.first,),
+                SizedBox(key:navbarKeys.first),
                 // Main
                 if(constraints.maxWidth >= kMinDesktopWidth)
-                  const HeaderDesktop() 
+                  HeaderDesktop(onNavMenuTap: (int navIndex){
+                    scrollToSection(navIndex);
+                  },) 
                 else
                 HeaderMobile(
                   onLogoTap: (){},
@@ -116,10 +112,24 @@ class _HomePageState extends State<HomePage> {
                 ),
             
                 // Footer
-                Footer(),
+                const Footer(),
               ],
             ),
           ));
     });
+  }
+
+  void scrollToSection(int navIndex){
+    if(navIndex == 4){
+      // open a blog page
+      js.context.callMethod('open',[SnsLinks.blog]);
+      return;
+    }
+
+    final key = navbarKeys[navIndex];
+    Scrollable.ensureVisible(
+      key.currentContext!,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.easeInOut);
   }
 }
--- lib/widgets/drawer_mobile.dart
+++ lib/widgets/drawer_mobile.dart
@@ -4,8 +4,8 @@ import '../constants/colors.dart';
 import '../constants/nav_items.dart';
 
 class DrawerMobile extends StatelessWidget {
-  const DrawerMobile({super.key});
-
+  const DrawerMobile({super.key, required this.onNavItemTap});
+  final Function(int) onNavItemTap;
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -31,7 +31,9 @@ class DrawerMobile extends StatelessWidget {
                   color: CustomColor.whitePrimary,
                   fontWeight: FontWeight.w600, 
                   fontSize: 16),
-                onTap: () {},
+                onTap: () {
+                  onNavItemTap(i);
+                },
                 leading: Icon(navIcons[i]),
                 title: Text(navTitles[i]),
               )
